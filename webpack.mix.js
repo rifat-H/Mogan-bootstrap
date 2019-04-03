2e96a2596ad6d4cfabb0814c26b3a7759b271aed
Rifat Hossain <admin@rhossain.me>
2019-04-03 20:31:49 +0600

    icons added

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,7 @@ const mix = require('laravel-mix');
 mix.js('src/js/script.js', 'dist/js')
     .sass('src/scss/bundle.scss', 'dist/css')
     .browserSync({
-        proxy: 'http://localhost/boilerplate/',
+        proxy: 'http://localhost/Mogan-bootstrap/',
         files: [
             'dist/css/bundle.css',
             './index.html'
